bcac0d5ba2ffa7af3bf9a956105d8081359c27d1
unknown <bogdan.pirjol123@gmail.com>
2023-10-16 16:06:07 +0300

    change logo symbol style and dimnesions

--- src/wrappers/Logo.js
+++ src/wrappers/Logo.js
@@ -1,25 +1,23 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-    display: flex;
-    align-items:center;
-    color: var(--budget-purple);
-    width: 241px;
-    height: 80px;
-    padding-right: 0;
-    gap: 10px;
-    flex-shrink: 0;
-    margin-left: 64px;
-    font-size: 28px;
-    font-weight: 500;
-    font-family: var(--baloo-font);
-    line-height: 43.79px;
-    font-weight: bold;
+  display: flex;
+  align-items: center;
+  color: var(--eb-3F51B5);
+  padding-right: 0;
+  gap: 8px;
+  flex-shrink: 0;
+  margin-left: 64px;
+  font-size: 32px;
+  font-weight: 500;
+  font-family: var(--baloo-font);
+  line-height: 43.79px;
+  
 
-    img {
-        width: 80px;
-        height: 80px;
-    }
+  img {
+    width: 92px;
+    height: 92px;
+  }
 `;
 
 export default Wrapper;
