e1f8f5fe84cc559ecacd46e4acc94df32d75c779
unknown <bogdan.pirjol123@gmail.com>
2023-10-16 20:20:20 +0300

    modify styles of Landing Page to ocupy all height of page

--- src/wrappers/Landing.js
+++ src/wrappers/Landing.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
     font-family: var(--roboto-font);
     min-width: fit-content;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
 `;
 
 export default Wrapper;
