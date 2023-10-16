8e1f0321a04cb74d4df1dff2486b541121a3f959
unknown <bogdan.pirjol123@gmail.com>
2023-10-16 20:19:41 +0300

    add footer component and integrate it in Landing Page

---
+++ src/components/LandingFooter.js
@@ -0,0 +1,17 @@
+import Wrapper from "../wrappers/LandingFooter";
+
+const Footer = () => {
+
+    return (
+        <Wrapper>
+            <p>Copyright ©2023 GeniusTech - All rights reserved</p>
+            <ul>
+                <li className="vertical-bar">Terms and conditions</li>
+                <li className="vertical-bar">Privacy Policy</li>
+                <li>Manage Cookies</li>
+            </ul>
+        </Wrapper>
+    )
+}
+
+export default Footer;
---
+++ src/wrappers/LandingFooter.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    background-color: var(--eb-F9F9F9);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    row-gap: 25px;
+    flex: 0 1 140px;
+    min-height: 120px;
+
+    p {
+        font-size: 24px;
+        font-family: var(--eb-17202A);
+        font-weight: 400;
+    } 
+
+    ul {
+        list-style-type: none;
+        display: flex;
+        li {
+            font-size: 24px;
+            font-weight: 500;
+        }
+    }
+
+    .vertical-bar::after {
+        content: '|';
+        padding: 8px;
+    }
+`;
+
+export default Wrapper; 
