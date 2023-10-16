3981fa2f97e09b41752f5d8f6e7f3e39ef377dde
unknown <bogdan.pirjol123@gmail.com>
2023-10-16 15:29:15 +0300

    add aditional modifications to fit main container of landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,13 @@
 import Wrapper from "../wrappers/Landing";
 import Navbar from "../components/LandingNavbar";
+import MainContainer from "../components/MainContainer";
 
 const Landing = () => {
     console.log(window.location);
     return (
         <Wrapper>
             <Navbar></Navbar>
+            <MainContainer/>
         </Wrapper>
     );
 }
--- src/wrappers/Landing.js
+++ src/wrappers/Landing.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
     font-family: var(--roboto-font);
+    min-width: fit-content;
 `;
 
 export default Wrapper;
