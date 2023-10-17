f21ad1df11a2846f75f898eaac79c56874fa4d86
unknown <bogdan.pirjol123@gmail.com>
2023-10-17 18:05:36 +0300

    add input component and style

---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import Wrapper from "../wrappers/Input";
+
+const Input = ({name, inputType, placeholder, className}) => {
+    return (
+        <Wrapper placeholder={placeholder} type={inputType} className={className}>
+
+        </Wrapper>
+    )
+}
+
+export default Input;
