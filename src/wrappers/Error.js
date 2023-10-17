eb5cbedd0907b00e7eeeab957713853b6ba1ac84
unknown <bogdan.pirjol123@gmail.com>
2023-10-17 22:18:40 +0300

    change error logo to svg for better perrforming in render

--- src/wrappers/Error.js
+++ src/wrappers/Error.js
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-   display: flex;
-
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  img {
+    min-width: 30vw;
+    max-width: 70vw;
+  }
   .message {
     font-family: var(--baloo-font);
     min-width: max-content;
     h1 {
-        font-weight: 100;
-        font-size: 48px;
+      font-weight: 100;
+      font-size: 48px;
     }
     align-self: center;
     .go-home {
