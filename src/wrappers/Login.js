8d54540596f67fdd1d6fff505d85eb164069fd2e
unknown <bogdan.pirjol123@gmail.com>
2023-10-17 17:42:08 +0300

    create login page

---
+++ src/wrappers/Login.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: var(--eb-80CBC4);;
+    div {
+        margin: 0;
+    }
+
+    .credentials-container {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-top: 33px;
+    }
+`;
+
+export default Wrapper;
