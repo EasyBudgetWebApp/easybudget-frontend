68b64179c157d102ff452156e77bdaafba6ae186
unknown <bogdan.pirjol123@gmail.com>
2023-11-15 16:57:54 +0200

    add loader screen for auth pages and andapt fetch utlity for OAuth links

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,9 @@
 import Wrapper from "../wrappers/Login";
 import LoginForm from "../components/LoginForm";
-import LoaderScreen from "../components/LoaderScreen";
 
 const Login = () => {
   return (
     <Wrapper className="auth-page">
-      <LoaderScreen className='cover-on-auth'/>
       <LoginForm></LoginForm>
     </Wrapper>
   );
